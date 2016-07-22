cc8ee5b2cff544e4379ce4c688fccb7da8bcf5ae
Steven Peercy <speercy@gmail.com>
2016-07-22 14:03:08 -0400

    Switching section placement in /hire

--- resources/views/pages/hire.blade.php
+++ resources/views/pages/hire.blade.php
@@ -9,16 +9,16 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-9">
-                    <section id="partners" class="m2-bottom">
-                        <h3 class="p1-top m0" @include('edit', ['key' => 'hire_partners_title'])>{!! $hire_partners_title !!}</h3>
-                        <hr>
-                        <div class="gallery p2-top" @include('edit', ['key' => 'hire_partners_images'])>{!! $hire_partners_images !!}</div>
-                    </section>
                     <section id="why" class="m2-top m2-bottom">
                         <hr>
                         <h3 class="m0" @include('edit', ['key' => 'hire_why_title'])>{!! $hire_why_title !!}</h3>
                         <hr>
                         <div @include('edit', ['key' => 'hire_why_desc'])>{!! $hire_why_desc !!}</div>
+                    </section>
+                    <section id="partners" class="m2-bottom">
+                        <h3 class="p1-top m0" @include('edit', ['key' => 'hire_partners_title'])>{!! $hire_partners_title !!}</h3>
+                        <hr>
+                        <div class="gallery p2-top" @include('edit', ['key' => 'hire_partners_images'])>{!! $hire_partners_images !!}</div>
                     </section>
                     <section id="questions" class="faqs m2-top m2-bottom">
                         <hr>
@@ -103,8 +103,8 @@
                     <nav class="subnav" data-spy="affix" data-offset-top="83" data-offset-bottom="753">
                         <h1 class="m0">{{ $title }}</h1>
                         <ul class="nav">
-                            <li><a href="#partners">Partners</a></li>
                             <li><a href="#why">Why partner?</a></li>
+                            <li><a href="#partners">Partners</a></li>
                             <li><a href="#questions">FAQs</a></li>
                             <li><a href="#form">Learn more</a></li>
                             <li><a href="/hire/graduates" class="button pink"><i class="fa fa-graduation-cap"></i><span>Grad List</span><span>Graduate List</span></a></li>

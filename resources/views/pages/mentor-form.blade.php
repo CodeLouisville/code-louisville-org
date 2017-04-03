fb07faac281b1b82cba51a59caa50f1aa2a306d7
bluerman <brian.luerman@gmail.com>
2017-04-03 10:47:55 -0400

    Adding swift Option to mentors

--- resources/views/pages/mentor-form.blade.php
+++ resources/views/pages/mentor-form.blade.php
@@ -80,6 +80,7 @@
                                                 <option value="php" @if (isset($mentor) && $mentor->track == 'php') selected @endif>PHP</option>
                                                 <option value="dotnet" @if (isset($mentor) && $mentor->track == 'dotnet') selected @endif>.NET</option>
                                                 <option value="ruby" @if (isset($mentor) && $mentor->track == 'ruby') selected @endif>Ruby</option>
+                                                <option value="swift" @if (isset($mentor) && $mentor->track == 'swift') selected @endif>Swift</option>
                                             </select>
                                         </label>
                                     </div>

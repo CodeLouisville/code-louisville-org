aadd13ec2acc53912b84a623ab50f2f590b34553
Steven Peercy <speercy@gmail.com>
2017-12-21 23:04:49 -0500

    Adds seed creation

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        Commands\CreateSeeds::class
     ];
 
     /**
@@ -24,8 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('seeds:create')->hourly();
     }
 
     /**
